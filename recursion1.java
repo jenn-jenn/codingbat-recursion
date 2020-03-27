c516134d2fa5643d923c7d406aa84072c4cab66b
Jennifer <jennwong1009@gmail.com>
2020-03-26 21:22:26 -0700

    adds strCopies

--- recursion1.java
+++ recursion1.java
@@ -355,6 +355,20 @@ public class recursion1 {
 
         return str;
     }
+    
+    public boolean strCopies(String str, String sub, int n) {
+        if (n == 0) {
+            return true;
+        }
+        if (str.length() < sub.length()) {
+            return false;
+        }
+
+        if (str.substring(0, sub.length()).equals(sub)) {
+            return strCopies(str.substring(1), sub, --n);
+        }
+        return strCopies(str.substring(1), sub, n);
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello");
